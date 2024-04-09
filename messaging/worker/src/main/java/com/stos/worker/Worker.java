d736de597200dd653c9aca3fda51fbaa2d1e1535
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2024-04-09 22:52:22 +0200

    [STOS-41]_worker_send_log

--- messaging/worker/src/main/java/com/stos/worker/Worker.java
+++ messaging/worker/src/main/java/com/stos/worker/Worker.java
@@ -5,14 +5,18 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.time.LocalTime;
+import java.util.Random;
 
 @Component
 public class Worker {
 
+    private static final Random random = new Random();
     private static final String TASK_QUEUE_NAME = "ev_tasks";
     private static final String TASK_EXCHANGE_NAME = "stos";
     private static final String TASK_ROUTING_KEY = "ev_files";
-    private static final String LOGGER_ROUTING_KEY = "logger";
+    private static final String LOGGER_ROUTING_KEY = "log";
+    private static final String FILE_SERVER_ROUTING_KEY = "files_server";
 
     private final Helpers helpers = new Helpers();
 
@@ -22,6 +26,21 @@ public class Worker {
 
     public void sendTaskMessage(byte[] message) {
         rabbitTemplate.convertAndSend(TASK_EXCHANGE_NAME, TASK_ROUTING_KEY, message);
+        System.out.println("Task message sent.");
+    }
+
+    public void sendLogs(byte[] logs){
+        rabbitTemplate.convertAndSend(TASK_EXCHANGE_NAME, LOGGER_ROUTING_KEY, logs);
+        System.out.println("Log sent.");
+    }
+
+    public void logEvent(String content, Messages.LogType logType){
+        Messages.LogEvent logEvent = Messages.LogEvent.newBuilder()
+                .setTime(LocalTime.now().toString())
+                .setType(logType)
+                .setContent(content)
+                .build();
+        sendLogs(logEvent.toByteArray());
     }
 
 
@@ -31,21 +50,20 @@ public class Worker {
             Messages.TaskDispatch task = helpers.deserializeTaskDispatch(message.getBody());
             processTask(task);
             sendTaskMessage(task.toByteArray());
-            System.out.println("Task processed and sent back");
         }
     }
 
     public void processTask(Messages.TaskDispatch task) {
         System.out.println("Processing task:");
         System.out.println(task);
-        requestFiles();
         processFiles();
-        sendFiles();
         try {
-            Thread.sleep(20);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        sendFiles();
+        logEvent("log from worker!!!", Messages.LogType.forNumber(random.nextInt(3)));
         System.out.println("Task processed.");
     }
 
@@ -55,19 +73,11 @@ public class Worker {
         }
     }
 
-    public void requestFiles(){
-        //todo
-    }
-
     public void sendFiles(){
         //todo
     }
 
     public void processFiles(){
-        //todo
-    }
-
-    public void sendLogs(){
         //todo
     }
 }
