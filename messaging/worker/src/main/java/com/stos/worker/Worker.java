8aaeb7503d848b14c19aa08230bc3be7ec8eb6da
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2024-04-10 16:05:27 +0200

    [STOS-43] worker-fileServer communication

--- messaging/worker/src/main/java/com/stos/worker/Worker.java
+++ messaging/worker/src/main/java/com/stos/worker/Worker.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Random;
 
 @Component
@@ -13,10 +14,10 @@ public class Worker {
 
     private static final Random random = new Random();
     private static final String TASK_QUEUE_NAME = "ev_tasks";
-    private static final String TASK_EXCHANGE_NAME = "stos";
+    private static final String EXCHANGE_NAME = "stos";
     private static final String TASK_ROUTING_KEY = "ev_files";
     private static final String LOGGER_ROUTING_KEY = "log";
-    private static final String FILE_SERVER_ROUTING_KEY = "files_server";
+    private static final String FILE_SERVER_ROUTING_KEY = "file_server";
 
     private final Helpers helpers = new Helpers();
 
@@ -25,13 +26,21 @@ public class Worker {
 
 
     public void sendTaskMessage(byte[] message) {
-        rabbitTemplate.convertAndSend(TASK_EXCHANGE_NAME, TASK_ROUTING_KEY, message);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, TASK_ROUTING_KEY, message);
         System.out.println("Task message sent.");
     }
 
     public void sendLogs(byte[] logs){
-        rabbitTemplate.convertAndSend(TASK_EXCHANGE_NAME, LOGGER_ROUTING_KEY, logs);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, LOGGER_ROUTING_KEY, logs);
         System.out.println("Log sent.");
+    }
+
+    public void sendFiles(List<Messages.File> dataList){
+        Messages.Files files = Messages.Files.newBuilder()
+                .addAllContent(dataList)
+                .setUuid("glowakota")
+                .build();
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, FILE_SERVER_ROUTING_KEY, files.toByteArray());
     }
 
     public void logEvent(String content, Messages.LogType logType){
@@ -42,7 +51,6 @@ public class Worker {
                 .build();
         sendLogs(logEvent.toByteArray());
     }
-
 
     public void receiveMessageRabbit() throws InvalidProtocolBufferException {
         Message message = rabbitTemplate.receive(TASK_QUEUE_NAME);
@@ -62,7 +70,7 @@ public class Worker {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        sendFiles();
+        sendFiles(task.getDataList());
         logEvent("log from worker!!!", Messages.LogType.forNumber(random.nextInt(3)));
         System.out.println("Task processed.");
     }
@@ -73,9 +81,7 @@ public class Worker {
         }
     }
 
-    public void sendFiles(){
-        //todo
-    }
+
 
     public void processFiles(){
         //todo
