ae11fba475b86442559c55c769f1223a094c0948
tpalinski <tpalinski@icloud.com>
2024-04-22 15:02:23 +0200

    [STOS-76] Add evaluator ingress

--- environment/k8s/deploy.sh
+++ environment/k8s/deploy.sh
@@ -2,7 +2,16 @@
 # Commands used for deployment of stos mock to k8s. Assumes microk8s is installed
 #
 alias mkctl="microk8s kubectl"
-mkctl apply -f ./evaluator-deployment.yaml
-mkctl apply -f ./evaluator-deployment.yaml
-mkctl expose deployment/evaluator
+# Install necessary addons
+microk8s enable dns
+microk8s enable dashboard
+microk8s enable registry
+microk8s enable ingress
+# Apply deployments
+microk8s kubectl apply -f ./evaluator-deployment.yaml
+microk8s kubectl apply -f ./worker-deployment.yaml
+# Networking
+microk8s kubectl expose deployment/evaluator
+microk8s kubectl apply -f ./ingress-controller.yaml
+# Start dashboard
 microk8s dashboard-proxy
